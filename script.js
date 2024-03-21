6545edabbf451df467f09e8a2efd9ec39a8d07f1
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-21 12:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype.jobGreet = function(name, age, jobtitle){
 	console.log(`Hello, my name is ${name}, I am ${age} years old, and mhy job title is ${jobtitle}`);
 }
+Object.setPrototypeOf(Employee.prototype, Person.prototype);
 
 
 // Do not change code below this line
