5e1f42f353e6dc918141ed91b22321de98ee94d8
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-21 18:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,29 +3,25 @@ function Person(name, age) {
 	this.name = name;
 	this.age = age;
 }
-Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+ 
+Person.prototype.greet = function () {
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 }
-
+ 
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
+	Person.call(this,name,age);
 	this.jobTitle = jobTitle;
-	
 }
-
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+ 
+Employee.prototype.jobGreet = function () {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
-
-
-
-// const employee = new Employee('Bob', 30, 'Accounts');
-// employee.greet();
-// employee.jobGreet();
-
+// const p = new Person('Alice',25);
+// p.greet();
+// const e = new Employee('Bob',30,'Manager');
+// e.jobGreet();
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
