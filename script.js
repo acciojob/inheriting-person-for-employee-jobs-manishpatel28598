07486b1dbd5665639ddc164dbe64c4456fe7f834
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-21 11:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.greet = function(name, age){
+	console.log(`Hello, my name is ${name}, I am ${age} years old.`);
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+	this.jobTitle = jobTitle;
+	
+}
+Employee.prototype.jobgreet = function(name, age, jobtitle){
+	console.log(`Hello, my name is ${name}, I am ${age} years old, and mhy job title is ${jobtitle}`);
+}
+
 
 // Do not change code below this line
 window.Person = Person;
